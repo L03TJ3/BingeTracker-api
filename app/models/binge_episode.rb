376ac66ba76667647ae5b0b592d1a0208510d14b
Laurence <laurenceweening@gmail.com>
2016-05-10 15:10:22 +0200

    Change validation & test on episods (season)

--- app/models/binge_episode.rb
+++ app/models/binge_episode.rb
@@ -1,4 +1,4 @@
 class BingeEpisode < ActiveRecord::Base
-  validates_presence_of :name, :description, :seasons
+  validates_presence_of :name, :description, :season
   belongs_to :binge_serie
 end
--- spec/models/binge_episode_spec.rb
+++ spec/models/binge_episode_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe BingeEpisode do
   it { should validate_presence_of   :name}
   it { should validate_presence_of   :description}
-  it { should validate_presence_of   :seasons}
+  it { should validate_presence_of   :season}
   it { should belong_to :binge_serie}
 end
