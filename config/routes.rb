b46841852d77cf6ea77dce3645ffc066baabcf92
Laurence <laurenceweening@gmail.com>
2016-05-11 15:45:41 +0200

    Install react_on_rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'hello_world', to: 'hello_world#index'
   resources :binge_series do
     namespace :season, path: ':season', module: nil do
       resources :binge_episodes, controller: 'binge_episodes'
