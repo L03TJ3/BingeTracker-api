a7f521e8c3131f96588f4f88d3a20915a1162481
Laurence <laurenceweening@gmail.com>
2016-05-11 23:12:49 +0200

    Add test for single episode in array

--- spec/models/binge_serie_spec.rb
+++ spec/models/binge_serie_spec.rb
@@ -8,9 +8,17 @@ describe BingeSerie do
 
   describe "seasons" do
     let(:series) { create(:binge_serie) }
+    let(:series1) { create(:binge_serie) }
+
+    let(:season) { 1 }
+    let!(:episode) { create(:binge_episode, season: season, binge_serie: series1)}
 
     it "expects an empty []" do
       expect(series.seasons).to eq []
+    end
+
+    it "expects a single episode" do
+      expect(series1.seasons).to eq [episode.season]
     end
   end
 
