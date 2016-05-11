f56637dc1f1a87e487d926d7c3dec994435dfa3b
Laurence <laurenceweening@gmail.com>
2016-05-11 21:51:12 +0200

    Add test for returning json format show page episodes (partially working)

--- spec/controllers/binge_episodes_controller_spec.rb
+++ spec/controllers/binge_episodes_controller_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 describe BingeEpisodesController do
   let(:binge_episode) { create(:binge_episode, season: "1") }
 
+  
+
   describe "GET show" do
     it "assigns single episode to instance variable @episode" do
       get :show, binge_series_id: binge_episode.binge_serie.to_param, season: "1", id: binge_episode.to_param
       expect(assigns(:episode)).to eq binge_episode
+    end
+
+    it "returns episodes in json format" do
+      get :show, binge_series_id: binge_episode.binge_serie.to_param, season: "1", id: binge_episode.to_param, format: :json
+
+      ## No strict: true here. schema doesn't validate with nested hashes/arrays
+      expect(response).to match_response_schema("binge_episode")
     end
   end
 end
