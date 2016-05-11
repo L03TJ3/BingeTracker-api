da2dfa5c0997356806cb68af87bc198f9eb7e6e1
Laurence <laurenceweening@gmail.com>
2016-05-11 19:11:22 +0200

    add to controller tests

--- spec/controllers/binge_episodes_controller_spec.rb
+++ spec/controllers/binge_episodes_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe BingeEpisodesController do
-  let(:binge_episode) { create(:binge_episode)}
+  let!(:binge_episode) { create(:binge_episode, season: "1") }
 
   describe "GET show" do
     it "assigns single episode to instance variable @episode" do
-      # get :show, binge_series_id: binge_episode.binge_serie.to_param, season: "1", id: binge_episode.to_param
+      get :show, binge_series_id: binge_episode.binge_serie.to_param, season: "1", id: binge_episode.to_param
+      expect(assigns(:episode)).to eq binge_episode
     end
   end
 end
--- spec/controllers/binge_series_controller_spec.rb
+++ spec/controllers/binge_series_controller_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 describe BingeSeriesController do
   describe "GET index" do
+    let!(:series) { create_list(:binge_serie, 3) }
+
     it "returns json format Series" do
       get :index, format: :json
       expect(response).to match_response_schema("binge_series")
+    end
+
+    it "should assign all series to @series" do
+      expect(assigns(:series)).to eq series
     end
   end
 end
